88513a5273740f402b339d5785394d624c7c4481
Francis Shonubi <shonubioladipo@gmail.com>
2022-04-24 03:19:19 +0100

    style: add Cart Item Card

---
+++ src/components/molecules/cart-image-card/CartImageCard.tsx
@@ -0,0 +1,19 @@
+import { Component } from "react";
+import CartItemImage from "../../../assets/images/cart-item-card-image.png";
+import AppButton from "../../atoms/app-button/AppButton";
+import "./CartImageCard.scss";
+
+export default class CartImageCard extends Component {
+  render() {
+    return (
+      <div id="cart-image-card">
+        <img src={CartItemImage} alt="cart item card image" />
+
+        <div className="image-btns">
+          <AppButton variant="secondary">{"<"}</AppButton>
+          <AppButton variant="secondary">{">"}</AppButton>
+        </div>
+      </div>
+    );
+  }
+}
