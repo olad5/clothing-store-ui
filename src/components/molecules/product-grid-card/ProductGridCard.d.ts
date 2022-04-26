ceea22e9425573f1bd52fb648ecb986393c50b39
Francis Shonubi <shonubioladipo@gmail.com>
2022-04-26 22:59:32 +0100

    style: add out of stock filter to product card

---
+++ src/components/molecules/product-grid-card/ProductGridCard.d.ts
@@ -0,0 +1,3 @@
+export type ProductGridCardProps = {
+  outOfStock?: boolean;
+};
--- src/components/molecules/product-grid-card/ProductGridCard.tsx
+++ src/components/molecules/product-grid-card/ProductGridCard.tsx
@@ -1,5 +1,7 @@
 import { Component } from "react";
 
+import { ProductGridCardProps } from "./ProductGridCard.d";
+
 // Components
 import PriceTag from "../../atoms/price-tag/PriceTag";
 import ProductItemImage from "../../../assets/images/product-A.png";
@@ -8,15 +10,21 @@ import AddToCartIcon from "../../atoms/add-to-cart-icon/AddToCartIcon";
 // Styles
 import "./ProductGridCard.scss";
 
-export default class ProductGridCard extends Component {
+export default class ProductGridCard extends Component<ProductGridCardProps> {
   render() {
     return (
-      <div id="product-grid-card">
+      <div
+        id="product-grid-card"
+        className={` ${this.props.outOfStock ? "out-of-stock" : ""} `}
+      >
         <img
           src={ProductItemImage}
           alt="Product Image"
           className="product-image"
         />
+        {this.props.outOfStock ? (
+          <div className="out-of-stock-text">out of stock</div>
+        ) : null}
         <p className="product-name">Apollo Running Street</p>
         <div className="price-tag">
           <PriceTag price={50} fontMedium={true} />
