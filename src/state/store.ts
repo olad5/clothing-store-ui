c6616f97d636a3691a1c00535a161ad3bf73a804
Francis Shonubi <shonubioladipo@gmail.com>
2022-05-17 11:06:43 +0100

    feat: save data to localStorage
    - Redux store data now saved to localStorage

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,6 +1,31 @@
 import thunk from "redux-thunk";
-import rootReducer from "./reducers";
-
+import rootReducer, { RootState } from "./reducers";
 import { createStore, applyMiddleware } from "redux";
 
-export const store = createStore(rootReducer, {}, applyMiddleware(thunk));
+function saveToLocalStorage(state: RootState) {
+  try {
+    const serialisedState = JSON.stringify(state);
+    localStorage.setItem("persistantState", serialisedState);
+  } catch (error) {
+    console.warn(error);
+  }
+}
+
+function loadFromLocalStorage() {
+  try {
+    const serialisedState = localStorage.getItem("persistantState");
+    if (serialisedState === null) return undefined;
+    return JSON.parse(serialisedState);
+  } catch (error) {
+    console.warn(error);
+    return undefined;
+  }
+}
+
+export const store = createStore(
+  rootReducer,
+  loadFromLocalStorage(),
+  applyMiddleware(thunk)
+);
+
+store.subscribe(() => saveToLocalStorage(store.getState()));
